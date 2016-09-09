5f46b576a17089a73c3676ae6cdfd7fa53be392b
Alan Platt <alan.platt@capablue.com>
2016-09-09 15:23:35 +0100

    Initial commit
    Basic working app

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='testapp',
+    packages=['testapp'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+    setup_requires=[
+        'pytest-runner',
+    ],
+    tests_require=[
+        'pytest',
+    ],
+)
